c92fffa363a6989488c086748b8d141bbd59a84d
v0807 <veralangeberg@gmail.com>
2025-11-13 10:32:42 +0100

    working on A2

--- code/assignment2/ner_machine_learning.py
+++ code/assignment2/ner_machine_learning.py
@@ -1,7 +1,14 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction import DictVectorizer
+from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import sys
+import csv
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+import numpy as np
+from scipy import sparse
+import pickle
 
 
 
@@ -36,50 +43,117 @@ def extract_embeddings_as_features_and_gold(conllfile,word_embedding_model):
 
 
 def extract_features_and_labels(trainingfile):
-    
+    # copied from A1, and altered
+    """Extract features and labels from the training file.
+    Arguments:
+        trainingfile: Path to the training data file.
+    Returns:
+        data: List of feature dictionaries for each token.
+        targets: List of target labels for each token.
+    """
     data = []
     targets = []
-    # TIP: recall that you can find information on how to integrate features here:
-    # https://scikit-learn.org/stable/modules/feature_extraction.html
+    sentence_position = 0
+    previous_POS = None
     with open(trainingfile, 'r', encoding='utf8') as infile:
         for line in infile:
             components = line.rstrip('\n').split()
             if len(components) > 0:
                 token = components[0]
-                feature_dict = {'token':token}
+                feature_dict = {'token':token, 'POS':components[1], 'case': 'uppercase' if token[0].isupper() else 'lowercase', 'contains_digits': any(char.isdigit() for char in token), 'position_in_sentence': sentence_position, 'previous_POS': previous_POS if previous_POS is not None else 'BOS'}
                 data.append(feature_dict)
-                #gold is in the last column
+                sentence_position += 1
+                previous_POS = components[1] #put the current POS as previous POS for the next token
                 targets.append(components[-1])
+            else:
+                sentence_position = 0
+                previous_POS = None
+                # gold NER-label is in the last column
+            
     return data, targets
     
 def extract_features(inputfile):
-   
+    # copied from A1, and altered/extended
+    """
+    Extract features from the input file.
+    Arguments:
+        inputfile: Path to the input data file.
+    Returns:
+        data: List of feature dictionaries for each token.
+    """
     data = []
+    sentence_position = 0
+    previous_POS = None
     with open(inputfile, 'r', encoding='utf8') as infile:
         for line in infile:
             components = line.rstrip('\n').split()
             if len(components) > 0:
                 token = components[0]
-                feature_dict = {'token':token}
+                feature_dict = {'token':token, 'POS':components[1], 'case': 'uppercase' if token[0].isupper() else 'lowercase', 'contains_digits': any(char.isdigit() for char in token), 'position_in_sentence': sentence_position, 'previous_POS': previous_POS if previous_POS is not None else 'BOS'}
                 data.append(feature_dict)
+                sentence_position += 1
+                previous_POS = components[1] #put the current POS as previous POS for the next token
+            else: # new sentence
+                sentence_position = 0
+                previous_POS = None
     return data
     
 def create_classifier(train_features, train_targets, modelname):
    
-    if modelname ==  'logreg':
-        # TIP: you may need to solve this: https://stackoverflow.com/questions/61814494/what-is-this-warning-convergencewarning-lbfgs-failed-to-converge-status-1
-        model = LogisticRegression()
-    vec = DictVectorizer()
-    features_vectorized = vec.fit_transform(train_features)
-    model.fit(features_vectorized, train_targets)
+    if modelname == 'logreg':
+        print("Creating Logistic Regression model")
+        logreg = LogisticRegression(max_iter=1001)
+        
+        # Vectorize features
+        print("Vectorizing features...")
+        vec = DictVectorizer(sparse=True)
+        features_vectorized = vec.fit_transform(train_features)
+        
+        # Scale features, otherwise it took ages to converge
+        print("Scaling features...")
+        scaler = StandardScaler(with_mean=False)  # False because we have sparse matrix
+        features_scaled = scaler.fit_transform(features_vectorized)
+        
+        print(f"Feature matrix shape: {features_scaled.shape}")
+        print(f"Number of samples: {len(train_targets)}")
+        
+        # Train model
+        print("Training model...")
+        model = logreg.fit(features_scaled, train_targets)
+        print("Training completed")
+        
+
+    if modelname == 'SVM':
+        svm = SVC()
+        vec = DictVectorizer()
+        features_vectorized = vec.fit_transform(train_features)
+        print("Training SVM model...")
+        model = svm.fit(features_vectorized, train_targets)
+        print("SVM training completed")
+
+    if modelname == 'NB':
+        nb = GaussianNB()
+        vec = DictVectorizer()
+        features_vectorized = vec.fit_transform(train_features).toarray()
+        model = nb.fit(features_vectorized, train_targets)
+
+    return model, vec, scaler if modelname == 'logreg' else None
+
     
-    return model, vec
-    
-    
-def classify_data(model, vec, inputdata, outputfile):
-  
+def classify_data(model, vec, scaler, inputdata, outputfile):
+    """
+    Classify input data and write predictions to output file.
+    Arguments:
+        model: Trained classification model.
+        vec: Fitted DictVectorizer.
+        inputdata: Path to input data file.
+        outputfile: Path to output file where predictions will be written.
+    Returns:
+        None (the results are written to the outputfile)
+    """
     features = extract_features(inputdata)
     features = vec.transform(features)
+    features = scaler.transform(features)
     predictions = model.predict(features)
     outfile = open(outputfile, 'w')
     counter = 0
@@ -104,6 +178,13 @@ def main(argv=None):
     
     #you can replace the values for these with paths to the appropriate files for now, e.g. by specifying values in argv
     #argv = ['mypython_program','','','']
+    # Copied from A1
+    data_folder = "../../data/conll2003/"
+    train_file = data_folder + "conll2003.train.conll"
+    test_file = data_folder + "conll2003.test.conll"
+    dev_file = data_folder + "conll2003.dev.conll"
+
+    argv = ['ner_machine_learning.py', train_file, dev_file, 'ner_output.conll']
     trainingfile = argv[1]
     inputfile = argv[2]
     outputfile = argv[3]
@@ -113,9 +194,22 @@ def main(argv=None):
     ## and make sure the path works correctly, or you can add an argument to the commandline that allows users to specify the location of the language model.
     
     training_features, gold_labels = extract_features_and_labels(trainingfile)
-    for modelname in ['logreg', 'NB', 'SVM']:
-        ml_model, vec = create_classifier(training_features, gold_labels, 'logreg')
-        classify_data(ml_model, vec, inputfile, outputfile.replace('.conll','.' + modelname + '.conll'))
+    for modelname in ['logreg', 'SVM']: #NB asks for too much memory on the test machine
+        ml_model, vec, scaler = create_classifier(training_features, gold_labels, modelname)
+        print(f"model and vec created for {modelname}")
+        with open(f"{modelname}_ner_model.pkl", "wb") as f:
+            pickle.dump(ml_model, f)
+        with open(f"{modelname}_vec.pkl", "wb") as f:
+            pickle.dump(vec, f)
+        if scaler is not None:
+            with open(f"{modelname}_scaler.pkl", "wb") as f:
+                pickle.dump(scaler, f)
+
+        if modelname == 'logreg':
+            classify_data(ml_model, vec, scaler, inputfile, outputfile.replace('.conll','.' + modelname + '.conll'))
+        else:
+            classify_data(ml_model, vec, None, inputfile, outputfile.replace('.conll','.' + modelname + '.conll'))
+        print("Done classifying with model:", modelname)
     
     
 if __name__ == '__main__':
